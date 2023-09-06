cf0766492e1f6d89adc0b8bf4f79eef677637146
Hrik Sinha <dailyhrik@gmail.com>
2023-09-06 23:49:47 +0530

    initial commit

--- quiz.js
+++ quiz.js
@@ -1,19 +1,18 @@
-var zodiacCheck = document.querySelector("#check-zodiac");
-var dateInput = document.querySelector("#date-of-birth");
-var zodiacName = document.querySelector("#output");
+var quizForm = document.querySelector("#quiz-form");
+var checkButton = document.querySelector("#check-button");
+var outputBox = document.querySelector("#output");
 
+var rightAnswers = ["Mutton Biriyani", "Cristiano Ronaldo", "Rabindra Sarobar", "Shreya", "Read Books", "Own Parents", "12 Rules For Life", "Never Give Up"]
 
-function zodiacNameCalculator(date) {
+checkButton.addEventListener("click", calculateScore);
 
-    var input = zodiacName.innerText;
-    return input
+function calculateScore() {
 
+    let score = 0;
+    let index = 0;
+
+    var formResults = new FormData(quizForm);
 
 }
-
-
-zodiacCheck.addEventListener("click", nameChecker);
-
-
 
 
