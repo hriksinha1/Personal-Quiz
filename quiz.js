8793733fd197e82112d1e6885c0bc69c17015e24
Hrik Sinha <dailyhrik@gmail.com>
2023-09-06 22:39:00 +0530

    initial commit

---
+++ quiz.js
@@ -0,0 +1,19 @@
+var zodiacCheck = document.querySelector("#check-zodiac");
+var dateInput = document.querySelector("#date-of-birth");
+var zodiacName = document.querySelector("#output");
+
+
+function zodiacNameCalculator(date) {
+
+    var input = zodiacName.innerText;
+    return input
+
+
+}
+
+
+zodiacCheck.addEventListener("click", nameChecker);
+
+
+
+
